3b30209c98f712a33958cfede83414656d7fb792
grandyd <61079545+grandyd@users.noreply.github.com>
2021-10-23 20:11:28 +0300

    upd

--- Peptide Encoding Problem/Source.cpp
+++ Peptide Encoding Problem/Source.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
 
-inline string translate(string pattern, map<string,char>& table, int len)
+inline string translate(string pattern, map<string, char>& table, int len)
 {
 	auto it = pattern.begin();
 	string buf;
@@ -14,24 +15,41 @@ inline string translate(string pattern, map<string,char>& table, int len)
 	int i = 0;
 	while (true)
 	{
-		
+
 		if (i % 3 == 0 && i != 0)
 		{
 			letters.push_back(table.find(buf)->second);
 			buf = "";
 		}
-		if (i == 3*len)
+		if (i == 3 * len)
 		{
 			break;
 		}
 		buf.push_back(*it);
 		i++;
 		it++;
-		
+
 	}
 	return letters;
 }
 
+inline string reverse_complement(string DNA)
+{
+	map<char, char> m;
+	m.emplace('C', 'G');
+	m.emplace('G', 'C');
+	m.emplace('T', 'A');
+	m.emplace('A', 'T');
+	string buf;
+	auto it = DNA.begin();
+	while (it != DNA.end())
+	{
+		buf.push_back(m.find(*it)->second);
+		it++;
+	}
+	reverse(buf.begin(), buf.end());
+	return buf;
+}
 
 int main()
 {
@@ -105,40 +123,36 @@ int main()
 	table.emplace("TTG", 'L');
 	table.emplace("TTT", 'F');
 
-	string DNA,peptide;
+	string DNA, peptide;
 	string buf;
 	cin >> DNA;
 	cin >> peptide;
 
-	
 
 
-	
+
+
 	int n = peptide.length();
 
 	auto it = DNA.begin();
 
-	string buffff;
-
-
-	for (int i = 0; it!=DNA.end(); i++,it++)
+	string pattern;
+	if (peptide.length() * 3 > DNA.length())
 	{
-		for (int j = i; j <i+ 6; j++)
-		{
-			buffff.push_back(DNA[j]);
-		}
-		cout << buffff<<endl;
-		buffff = "";
-		/*if (i%(n*3)==0 && it!=DNA.begin())
-		{
-			cout << buf<<endl;
-			if (translate(buf, table,n) == peptide)
-			{
-				cout << buf << " ";
-			}
-			buf = "";
-		}
-		buf.push_back(*it);*/
+		return 0;
 	}
 
+	for (int i = 0; i < DNA.length() - (3 * n) + 1; i++, it++)
+	{
+		for (int j = i; j < i + 3 * n; j++)
+		{
+			pattern.push_back(DNA[j]);
+		}
+
+		if (translate(pattern, table, n) == peptide || translate(reverse_complement(pattern), table, n) == peptide)
+		{
+			cout << pattern << "\n";
+		}
+		pattern = "";
+	}
 }
